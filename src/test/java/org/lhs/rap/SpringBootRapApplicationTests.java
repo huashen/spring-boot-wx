d4f40dd0399e4048be815aff827d3555be48d167
huashen <huashen361@gmail.com>
2017-04-12 23:57:10 +0800

    git init

---
+++ src/test/java/org/lhs/rap/SpringBootRapApplicationTests.java
@@ -0,0 +1,16 @@
+package org.lhs.rap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
